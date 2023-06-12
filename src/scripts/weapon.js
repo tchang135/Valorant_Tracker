1f2bad886fa2aae75c756b198cb57b3b1efe8a63
Timothy Chang <tchang135@gmail.com>
2023-06-12 13:11:33 -0400

    adding logic for weapons section

--- src/scripts/weapon.js
+++ src/scripts/weapon.js
@@ -1,5 +1,29 @@
-class Weapon {
-    constructor(data) {
-        this.name = data.name;
+function findWeaponData (weaponData, weaponName) {
+    let weapon =""
+    for (let i = 0; i < weaponData.data.length; i++) {
+        if (weaponData.data[i].displayName.toUpperCase() === weaponName.toUpperCase() && weaponData.data[i]["isPlayableCharacter"]) {
+            weapon = weaponData.data[i]
+            break;
+        }
     }
+
+    if (weapon) {
+        
+    }
+}
+
+export function weaponDataFetch(weaponName) {
+    fetch("https://valorant-api.com/v1/weapons")
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        const weaponData = data;
+        findWeaponData (weaponData, weaponName)
+        const weaponOptions = document.querySelector('#weaponOptions');
+        weaponOptions.addEventListener("change", (e) => {
+            const weaponName2 = e.target.value;
+            findWeaponData( weaponData, weaponName2)
+        })
+    })
 }
